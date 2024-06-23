728141e5bb3dc86d144196e39daa1da976395327
Pedro Nelson <nelson.rreis@outlook.com>
2024-06-23 14:39:42 -0300

    Adicionado nova funcao para realizar busca em arquivo xml

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request
 import json
 import urllib.request
+import xml.etree.ElementTree as ET
 
 #Rota INDEX - Pagina Principal 
 @app.route('/')
@@ -58,3 +59,19 @@ def salvar_preco():
     print(f'Unidade:{preco_unitario}')
     print(f'Atacado:{preco_atacado}')
     return render_template('index.html')
+
+@app.route('/codigo', methods=['GET'])
+def consultar_produto_por_codigo_barra():
+    codigo_barra = request.args.get('codigo_barras')
+    tree = ET.parse('produtos.xml')
+    xml_root = tree.getroot()
+    for produto in xml_root.findall('produto'):
+        for embalagem in produto.findall('embalagem'):
+            if embalagem.find('codigo_barra').text == codigo_barra:
+                codig_interno = produto.get('sku')
+                descricao = produto.find('descricao').text
+                complemento = produto.find('complemento').text
+                
+                return render_template('buscar.html', descricao=descricao, codigo_barras=codigo_barra, codig_interno=codig_interno, complemento=complemento)
+    
+    return None
