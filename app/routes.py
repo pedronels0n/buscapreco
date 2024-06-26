9861330591fa3c283eb51886cfff8c44466bb3dc
Pedro Nelson <nelson.rreis@outlook.com>
2024-06-25 21:20:48 -0300

    realizados alguns testes de db porem sem sucesso

--- app/routes.py
+++ app/routes.py
@@ -23,56 +23,46 @@ def contato():
 @app.route('/buscar', methods=['GET'])
 #Criando funcao para receber o codigo enviado via GET
 #da pagina Index.html
-def buscador():
-
-    codigo_barras = request.args.get('codigo_barras')
-    
-    #API para Codigo de Barras
-    #Configuracao de API da Cosmos
-    headers = {
-          'X-Cosmos-Token': 'WN0U_hsymg5d1x8_7XKwDQ',
-          'Content-Type': 'application/json',
-          'User-Agent': 'Cosmos-API-Request'
-      }
-
-    req      = urllib.request.Request(f'https://api.cosmos.bluesoft.com.br/gtins/{codigo_barras}.json', None, headers)
-    response = urllib.request.urlopen(req)
-    data     = json.loads(response.read().decode('utf-8'))
-    #Filtrando as informacoes Essenciais
-    #para retonar ao Cliente
-    descricao = data['description']
-    foto = data['thumbnail']
-
-    return render_template('buscar.html', descricao=descricao, codigo_barras=codigo_barras, foto=foto)
 
 @app.route('/salvar_precos', methods=['POST'])
 def salvar_preco():
     #Capturando dos dados do formulario
-    codigo_barras = request.form.get('codigo_barras_hidden')
+    codigo_barras = str(request.form.get('codigo_barras_hidden'))
+    codigo_interno = str(request.form.get('codigo_interno'))
+    complemento = str(request.form.get('complemento'))
     preco_unitario = float(request.form.get('preco_unitario').replace(',', '.'))
     preco_atacado = float(request.form.get('preco_atacado').replace(',', '.'))
-    descricao = request.form.get('descricao')
+    descricao = str(request.form.get('descricao'))
 
 
     print(f'Descricao:{descricao}')
+    print(f'SKU:{codigo_interno}')
+    print(f'Complemento: {complemento}')
     print(f'Codigo:{codigo_barras}')
     print(f'Unidade:{preco_unitario}')
     print(f'Atacado:{preco_atacado}')
     return render_template('index.html')
 
+
+#Rota para buscar o codigo de barras fornecido
 @app.route('/codigo', methods=['GET'])
 def consultar_produto_por_codigo_barra():
+    #Utilizamos um arquivo xml como fonte de dados
+    #para obter informacoes essenciais do produto
     codigo_barra = request.args.get('codigo_barras')
     tree = ET.parse('produtos.xml')
     xml_root = tree.getroot()
     for produto in xml_root.findall('produto'):
         for embalagem in produto.findall('embalagem'):
-            if embalagem.find('codigo_barra').text == codigo_barra:
+            if embalagem.find('codigo_barras').text == codigo_barra:
                 codig_interno = produto.get('sku')
                 descricao = produto.find('descricao').text
                 complemento = produto.find('complemento').text
                 
-                return render_template('buscar.html', descricao=descricao, codigo_barras=codigo_barra, codig_interno=codig_interno, complemento=complemento)
+                return render_template('buscar.html', descricao=descricao, codigo_barras=codigo_barra, codigo_interno=codig_interno, complemento=complemento)
+            #Caso nao contenha no arquivo
+            #criamos uma contigencia utilizando 
+            #uma api da cosmos para buscar a descricao do produto
             else:
                 codigo_barras = request.args.get('codigo_barras')
     
@@ -90,7 +80,6 @@ def consultar_produto_por_codigo_barra():
                 #Filtrando as informacoes Essenciais
                 #para retonar ao Cliente
                 descricao = data['description']
-                foto = data['thumbnail']
 
                 return render_template('buscar.html', descricao=descricao, codigo_barras=codigo_barras)
     return None
